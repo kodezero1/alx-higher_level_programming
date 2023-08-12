4336b483791bd500c2355c4d6baf44739bf4442b
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-08-12 08:06:34 +0100

    moved import to the top

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from add_0 import add
+if __name__ == "__main__":
+    """This prints the sum of 1 & 2"""
+
+    a = 1
+    b = 2
+  
+    print("{} + {} = {}".format(a, b, add(a, b)))
+  #Prints the sum
