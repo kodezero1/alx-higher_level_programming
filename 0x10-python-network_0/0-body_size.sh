adbb593abe9e21e720a48d354c5a0ca8d7abd2f7
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-12-03 14:04:30 +0100

    style(bash): Improve script for getting body size of a request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ends a request to that URL displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
